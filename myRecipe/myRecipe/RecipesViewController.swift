9387f70786a5e8e8a752a2317566ff68c4191f92
Xueyan Xie <xueyan.xie@wustl.edu>
2017-11-23 16:05:28 -0600

    Added Collection View and finished search.

--- myRecipe/myRecipe/RecipesViewController.swift
+++ myRecipe/myRecipe/RecipesViewController.swift
@@ -9,9 +9,11 @@
 
 import UIKit
 
-class RecipesViewController: UIViewController, UISearchBarDelegate {
+class RecipesViewController: UIViewController, UISearchBarDelegate, UICollectionViewDataSource, UICollectionViewDelegate {
     
     @IBOutlet weak var searchBar: UISearchBar!
+    
+    @IBOutlet weak var theCollectionView: UICollectionView!
     
     var theData: [Info] = []
     var theImageCache: [String: UIImage] = [:]
@@ -22,14 +24,25 @@ class RecipesViewController: UIViewController, UISearchBarDelegate {
         // Do any additional setup after loading the view, typically from a nib.
         searchBar.delegate = self;
     }
+    
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         theData = [];
-//        theCollectionView.reloadData();
+        theCollectionView.reloadData();
         if(searchText != "") {
             self.fetchDataCollectionView(query: searchText);
         }
     }
 
+    // 2 cells per row.
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "recipeCell", for: indexPath);
+        return cell;
+    }
+    
     // getJSON from class demo 2.
     private func getJSON(_ url:String) -> JSON {
         if let url = URL(string: url) {
@@ -44,14 +57,32 @@ class RecipesViewController: UIViewController, UISearchBarDelegate {
         }
         
     }
-    
+
+    func cacheImages() {
+        for item in theData {
+            let url = URL(string: item.url)
+            let recipeID = item.recipeID
+            
+            if (url != nil) {
+                let data = try? Data(contentsOf: url!)
+                if (data != nil) {
+                    let image = UIImage(data: data!)
+                    theImageCache[recipeID] = image
+                }
+            }
+        }
+    }
+
+    // From Lab 4.
     func fetchDataCollectionView(query: String) {
         let formatQuery = query.replacingOccurrences(of: " ", with: "&");
         let json = getJSON(urlPrefix + formatQuery)
         for result in json["meals"].arrayValue {
-            print (result)
-            let id = result["idMeal"].stringValue
-            print (id)
+            let id = result["idMeal"].stringValue;
+            let name = result["strMeal"].stringValue;
+            let url = result["strMealThumb"].stringValue;
+            theData.append(Info(name: name, recipeID: id, url: url))
         }
+        cacheImages();
     }
 }
